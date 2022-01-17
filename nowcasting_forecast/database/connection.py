54cf246ea62d435c79374c5e42b7410f61afd2e5
peterdudfield <peter.dudfield@hotmail.com>
2022-01-17 17:07:13 +0000

    add create tables method when making database connection

--- nowcasting_forecast/database/connection.py
+++ nowcasting_forecast/database/connection.py
@@ -2,7 +2,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from nowcasting_forecast.database.models import Base
+import logging
 
+logger = logging.getLogger(__name__)
 
 class DatabaseConnection:
     """Database connection class"""
@@ -16,6 +19,15 @@ class DatabaseConnection:
         self.url = url
 
         self.engine = create_engine(self.url, echo=True)
+
+        # quick and easy way to make sure the database table names are made.
+        # This should be moved, or done separate from here.
+        try:
+            Base.metadata.create_all(self.engine)
+        except Exception as e:
+            logger.debug(f'Try to run "create all" on database, but failed. '
+                         f'Will pass this anyway {e}')
+
         self.Session = sessionmaker(bind=self.engine)
 
     def get_session(self) -> Session:
