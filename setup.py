91b4e77bdf15af5b9810b4d77e6ef95edc5c3b00
BumpVersion Action <bumpversion@github-actions>
2022-01-20 11:19:11 +0000

    Bump version: 0.0.16 → 0.0.17

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="nowcasting_forecast",
     packages=find_packages(),
-    version="0.0.16",
+    version="0.0.17",
     license="MIT",
     description="Live forecast for the OCF nowcasting project",
     author="Peter Dudfield",
