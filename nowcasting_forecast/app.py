bc1a0e97f5f547c06fe033669efad2b48599cc32
peterdudfield <peter.dudfield@hotmail.com>
2022-02-04 10:51:45 +0000

    fix for dummy test

--- nowcasting_forecast/app.py
+++ nowcasting_forecast/app.py
@@ -12,6 +12,7 @@ from nowcasting_forecast.database.fake import make_fake_forecasts, make_fake_nat
 from nowcasting_forecast.database.save import save
 from nowcasting_forecast.models.nwp_solar_simple import nwp_irradiance_simple_run_all_batches
 from nowcasting_forecast.utils import floor_30_minutes_dt
+from sqlalchemy.orm import Session
 
 logger = logging.getLogger(__name__)
 
@@ -44,7 +45,7 @@ def run(db_url: str, fake: bool = False):
     with connection.get_session() as session:
 
         if fake:
-            forecasts = make_dummy_forecasts()
+            forecasts = make_dummy_forecasts(session=session)
         else:
 
             with tempfile.TemporaryDirectory() as temporary_dir:
@@ -60,7 +61,7 @@ def run(db_url: str, fake: bool = False):
         save(forecasts=forecasts, session=session)
 
 
-def make_dummy_forecasts():
+def make_dummy_forecasts(session:Session):
     """Make dummy forecasts
 
     Dummy forecasts are made for all gsps and for several forecast horizons
@@ -69,7 +70,7 @@ def make_dummy_forecasts():
     t0_datetime_utc = floor_30_minutes_dt(datetime.utcnow())
 
     # make gsp fake results
-    forecasts = make_fake_forecasts(gsp_ids=list(range(N_GSP)), t0_datetime_utc=t0_datetime_utc)
+    forecasts = make_fake_forecasts(gsp_ids=list(range(N_GSP)), t0_datetime_utc=t0_datetime_utc, session=session)
 
     # add national forecast
     forecasts.append(make_fake_national_forecast(t0_datetime_utc=t0_datetime_utc))
