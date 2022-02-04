a6f1efbd59f532d9713893b7286a71046a38eb6f
peterdudfield <peter.dudfield@hotmail.com>
2022-02-04 10:22:53 +0000

    add test bk in

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,9 +1,14 @@
-import pytest
-
+import tempfile
+import os
 from nowcasting_forecast.batch import make_batches
 
 
-@pytest.mark.skip("Skip for now #11")
-def test_make_batches():
+def test_make_batches(nwp_data):
 
-    make_batches()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # save nwp data
+        nwp_path = f"{temp_dir}/unittest.netcdf"
+        nwp_data.to_netcdf(nwp_path, engine="h5netcdf")
+        os.environ["NWP_ZARR_PATH"] = nwp_path
+
+        make_batches()
