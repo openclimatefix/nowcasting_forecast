1ff0217b522e78263ff027cf84063569b69eca3b
peterdudfield <peter.dudfield@hotmail.com>
2022-02-14 17:16:38 +0000

    add error test

--- tests/models/nwp_simple_trained/test_model.py
+++ tests/models/nwp_simple_trained/test_model.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 import torch
+import pytest
 
 from nowcasting_forecast.models.nwp_simple_trained.model import Model
 
@@ -19,6 +20,13 @@ def test_model_load_weights():
         torch.save({"state_dict": model.state_dict()}, model_nwp_simple_trained_weights)
 
         model.load_model(remote_filename=model_nwp_simple_trained_weights)
+
+
+def test_model_load_weights_error():
+    model = Model()
+
+    with pytest.raises(Exception):
+        model.load_model(remote_filename="weights.ckpt")
 
 
 def test_forward():
