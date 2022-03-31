131fd59810b506359dc04d0ced0b225f26845658
peterdudfield <peter.dudfield@hotmail.com>
2022-03-31 13:55:55 +0200

    tidy app

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import tempfile
 import xarray as xr
 from click.testing import CliRunner
 from nowcasting_datamodel.connection import DatabaseConnection
-from nowcasting_datamodel.models import Forecast, ForecastSQL, LocationSQL, national_gb_label
+from nowcasting_datamodel.models import Forecast, ForecastSQL, LocationSQL
 
 from nowcasting_forecast.app import run
 
@@ -20,9 +20,8 @@ def test_fake(db_connection: DatabaseConnection):
         _ = Forecast.from_orm(forecasts[0])
         assert len(forecasts) == 338 + 1  # 338 gsp + national
 
-        locations = session.query(LocationSQL).order_by(LocationSQL.gsp_id).all()
+        locations = session.query(LocationSQL).all()
         assert len(locations) == 338 + 1
-        assert locations[0].label == national_gb_label
 
 
 def test_not_fake(db_connection: DatabaseConnection, nwp_data: xr.Dataset):
@@ -41,10 +40,5 @@ def test_not_fake(db_connection: DatabaseConnection, nwp_data: xr.Dataset):
         with db_connection.get_session() as session:
             forecasts = session.query(ForecastSQL).all()
             _ = Forecast.from_orm(forecasts[0])
-            # assert len(forecasts) == 338 + 1  # 338 gsp + national
+            assert len(forecasts) == 338 + 1  # 338 gsp + national
             assert len(forecasts[0].forecast_values) > 1
-
-            locations = session.query(LocationSQL).order_by(LocationSQL.gsp_id).all()
-            assert len(locations) == 338 + 1
-            assert locations[0].label == national_gb_label
-            assert locations[0].gsp_id == 0
