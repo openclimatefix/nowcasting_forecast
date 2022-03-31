c438d8be172e9343b99bcf82b778830a3e630ac3
peterdudfield <peter.dudfield@hotmail.com>
2022-03-31 13:57:37 +0200

    update

--- nowcasting_forecast/models/nwp_simple_trained/nwp_simple_trained.py
+++ nowcasting_forecast/models/nwp_simple_trained/nwp_simple_trained.py
@@ -24,7 +24,8 @@ from nowcasting_datamodel.models import (
     InputDataLastUpdatedSQL,
 )
 from nowcasting_datamodel.national import make_national_forecast
-from nowcasting_datamodel.read.read import (  # get_latest_input_data_last_updated,
+from nowcasting_datamodel.read.read import (
+    get_latest_input_data_last_updated,
     get_location,
     get_model,
 )
@@ -75,8 +76,7 @@ def nwp_irradiance_simple_trained_run_all_batches(
     # make dataloader
     dataloader = iter(BatchDataLoader(n_batches=n_batches, configuration=configuration))
 
-    # input_data_last_updated = get_latest_input_data_last_updated(session=session)
-    input_data_last_updated = make_fake_input_data_last_updated()
+    input_data_last_updated = get_latest_input_data_last_updated(session=session)
 
     # load model
     model = Model()
@@ -149,7 +149,6 @@ def nwp_irradiance_simple_trained_run_one_batch(
     model = get_model(name=NAME, version=nowcasting_forecast.__version__, session=session)
 
     if input_data_last_updated is None:
-        # TODO make input data from actual data
         input_data_last_updated = make_fake_input_data_last_updated()
 
     # run model
