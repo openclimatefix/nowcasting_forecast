02cdcd64d5695a8b783c36a89b302192acd810bf
Jacob Bieker <jacob@bieker.tech>
2022-04-28 16:18:17 +0100

    Update batch tests

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -31,13 +31,16 @@ def test_make_batches_mvp_v1(nwp_data, pv_yields_and_systems):
         )
 
 
-def test_make_batches_mvp_v2(nwp_data, pv_yields_and_systems):
+def test_make_batches_mvp_v2(nwp_data, pv_yields_and_systems, sat_data):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         # save nwp data
         nwp_path = f"{temp_dir}/unittest.netcdf"
         nwp_data.to_netcdf(nwp_path, engine="h5netcdf")
         os.environ["NWP_PATH"] = nwp_path
+        sat_path = f"{temp_dir}/sat_unittest.netcdf"
+        sat_data.to_netcdf(sat_path, engine="h5netcdf")
+        os.environ["HRV_SAT_PATH"] = sat_path
 
         make_batches(
             config_filename="nowcasting_forecast/config/mvp_v2.yaml", temporary_dir=temp_dir
@@ -60,4 +63,6 @@ def test_make_sat_batches(sat_data):
         sat_data.to_netcdf(nwp_path, engine="h5netcdf")
         os.environ["HRV_SAT_PATH"] = nwp_path
 
-        make_batches()
+        make_batches(
+            config_filename="nowcasting_forecast/config/mvp_v2.yaml", temporary_dir=temp_dir
+        )
