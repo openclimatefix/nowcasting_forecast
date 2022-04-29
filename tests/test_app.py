11efdf49699ace690aed271b367166d54093d6e2
Jacob Bieker <jacob@bieker.tech>
2022-04-29 08:24:45 +0100

    Fix config

--- tests/test_app.py
+++ tests/test_app.py
@@ -42,23 +42,3 @@ def test_not_fake(db_connection: DatabaseConnection, nwp_data: xr.Dataset):
             _ = Forecast.from_orm(forecasts[0])
             assert len(forecasts) == 338 + 1  # 338 gsp + national
             assert len(forecasts[0].forecast_values) > 1
-
-
-def test_not_fake_sat(db_connection: DatabaseConnection, sat_data: xr.Dataset):
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-
-        # save nwp data
-        sat_path = f"{temp_dir}/unittest.netcdf"
-        sat_data.to_netcdf(sat_path, engine="h5netcdf")
-        os.environ["HRV_SAT_PATH"] = sat_path
-
-        runner = CliRunner()
-        response = runner.invoke(run, ["--db-url", db_connection.url, "--fake", "false"])
-        assert response.exit_code == 0, response
-
-        with db_connection.get_session() as session:
-            forecasts = session.query(ForecastSQL).all()
-            _ = Forecast.from_orm(forecasts[0])
-            assert len(forecasts) == 338 + 1  # 338 gsp + national
-            assert len(forecasts[0].forecast_values) > 1
