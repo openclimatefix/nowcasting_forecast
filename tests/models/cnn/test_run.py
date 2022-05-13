ab8be5bde732942e91112bfcc4048c28119eaa38
Jacob Bieker <jacob@bieker.tech>
2022-05-13 12:11:16 +0100

    Fix path

--- tests/models/cnn/test_run.py
+++ tests/models/cnn/test_run.py
@@ -32,7 +32,9 @@ def test_run(nwp_data, pv_yields_and_systems, sat_data, hrv_sat_data, db_session
         os.environ["SAT_PATH"] = sat_path
 
         make_batches(
-            config_filename="/home/jacob/Development/nowcasting_forecast/nowcasting_forecast/config/mvp_v2.yaml", temporary_dir=temp_dir
+            config_filename=os.path.join(
+                os.path.dirname(nowcasting_forecast.__file__), "config", "mvp_v2.yaml"
+            ), temporary_dir=temp_dir
         )
 
         dataloader = get_cnn_data_loader(src_path=os.path.join(temp_dir, "live"), tmp_path=os.path.join(temp_dir, "live"))
