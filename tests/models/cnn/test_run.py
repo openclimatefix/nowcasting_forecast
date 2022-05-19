9a8907c3772bac7b5add7e2c4da79c5e95e6da21
peterdudfield <peter.dudfield@hotmail.com>
2022-05-19 22:21:53 +0100

    add freeze time

--- tests/models/cnn/test_run.py
+++ tests/models/cnn/test_run.py
@@ -3,6 +3,7 @@
 import os
 import tempfile
 
+from freezegun import freeze_time
 import xarray as xr
 import zarr
 
@@ -14,6 +15,7 @@ from nowcasting_forecast.models.cnn.model import Model
 from nowcasting_forecast.models.utils import general_forecast_run_all_batches
 
 
+@freeze_time('2022-01-01 12:00')
 def test_run(
     nwp_data, pv_yields_and_systems, sat_data, hrv_sat_data, db_session, input_data_last_updated
 ):
