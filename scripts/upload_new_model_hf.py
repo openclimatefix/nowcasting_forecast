cf6c67896a84c0d1cddbba41345b0df427cc04bf
peterdudfield <peter.dudfield@hotmail.com>
2022-06-20 14:45:04 +0100

    example script to upload to hf

---
+++ scripts/upload_new_model_hf.py
@@ -0,0 +1,12 @@
+"""
+log in to the HF command line then run this command to upload a new model
+'hugginface-cli login' (might need to do pip install huggingface_hub)
+The whole process might take about 5 mins
+"""
+
+from nowcasting_forecast.models.cnn.model import Model
+
+filename = "results_w_r1042_e4.ckpt"
+
+model = Model.load_from_checkpoint(filename)
+model.push_to_hub("nowcasting_cnn_v2", organization="openclimatefix")
