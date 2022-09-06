d705d0199d9b30ac70d55811545589f81e039deb
Jacob Bieker <jacob@bieker.tech>
2022-09-06 08:29:27 +0100

    Add test for power perceiver dataloader

---
+++ tests/models/power_perceiver/test_power_perceiver_dataloader.py
@@ -0,0 +1,14 @@
+from nowcasting_forecast.models.power_perceiver.dataloader import get_power_perceiver_data_loader
+import os
+import tempfile
+import nowcasting_forecast
+
+def test_get_power_perceiver_data_loader():
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # make configuration
+        configuration_file = os.path.join(
+            os.path.dirname(nowcasting_forecast.__file__), "config", "mvp_v3.yaml"
+        )
+
+        _ = get_power_perceiver_data_loader(configuration_file=configuration_file)
