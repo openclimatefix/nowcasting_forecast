a776c4dad13ea82737e7acbf3230c4d7c71c059a
peterdudfield <peter.dudfield@hotmail.com>
2022-12-11 22:36:00 +0000

    remove timezone

--- nowcasting_forecast/batch.py
+++ nowcasting_forecast/batch.py
@@ -25,7 +25,7 @@ def make_batches(
     logger.info(f"Making batches using configuration file: {config_filename}")
 
     if t0_datetime_utc is None:
-        t0_datetime_utc = floor_minutes_dt(datetime.now(tz=timezone.utc))  # add timezone
+        t0_datetime_utc = floor_minutes_dt(datetime.utcnow()) 
         
         # add one second, this makes sure the most recent gsp data is used
         t0_datetime_utc = t0_datetime_utc + timedelta(seconds=1)
