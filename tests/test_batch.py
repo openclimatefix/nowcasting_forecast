2c541debf15ffed7f00c6327063b13d009d80de7
peterdudfield <peter.dudfield@hotmail.com>
2022-12-15 09:47:09 +0000

    floor

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -121,7 +121,7 @@ def test_make_batches_pvnet_v1(
         assert (
             pd.to_datetime(pv.time.values[0, -1]).isoformat()
             == pd.Timestamp(datetime.now(tz=timezone.utc))
-            .ceil("5T")
+            .floor("5T")
             .replace(tzinfo=None)
             .isoformat()
         )
